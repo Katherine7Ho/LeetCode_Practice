f42f6e15a6819131f8b83d74a545eb0fbfebdadd
Katherine Ho <91734878+Katherine7Ho@users.noreply.github.com>
2022-02-06 19:23:57 +0800

    Update TwoSum.py

--- Easy/TwoSum/TwoSum.py
+++ Easy/TwoSum/TwoSum.py
@@ -5,3 +5,20 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+"""Others Solution"""
+"""credit by HankLiu5"""
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+             else:
+                return [h[n], i]
